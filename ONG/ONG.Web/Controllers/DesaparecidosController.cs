636fc9b86251cf6166552ee694c5716c796f0521
DELL <DELL@DESKTOP-PV5V4EA>
2021-04-13 09:25:29 -0600

    sincronizando datos

---
+++ ONG/ONG.Web/Controllers/DesaparecidosController.cs
@@ -0,0 +1,26 @@
+﻿using ONG.BL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ONG.Web.Controllers
+{
+    public class DesaparecidosController : Controller
+    {
+       
+        // GET: Desaparecidos
+        public ActionResult Index()
+        {
+            var _desaparecidosBL = new DesaparecidosBL();
+            var listadeDesaparecidos = _desaparecidosBL.ObtenerDesaparecidosActivos();
+
+            ViewBag.adminWebsiteUrl = ConfigurationManager.AppSettings["adminWebsiteUrl"];
+
+            return View(listadeDesaparecidos);
+    
+        }
+    }
+}
---
+++ ONG/ONG.Web/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using ONG.BL;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ONG.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var beneficiosBL = new BeneficiosBL();
+            var listadeBeneficios = beneficiosBL.ObtenerBeneficios();
+
+            ViewBag.adminWebsiteUrl = ConfigurationManager.AppSettings["adminWebsiteUrl"];
+
+
+            return View(listadeBeneficios);
+        }
+    }
+}
